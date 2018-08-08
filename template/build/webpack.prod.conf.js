a9de3a5f0e61010ab03a38aa483681749da6e808
Raul Hernandez <39533201+rahch1982@users.noreply.github.com>
2018-08-08 11:44:09 -0400

    Exclude deprecated dependencies
    Replacement of deprecated library "extract-text-webpack-plugin", which was responsible for getting the css and scss files bundled and then getting the resulting css included within the <style> tags, before Webpack 4.
    
    This library was replaced by "mini-css-extract-plugin", which is meant to replace"extract-text-webpack-plugin" to provide Webpack 4 support, and better compatibility on future versions.
    
    Additionally "postcss-safe-parser" its included (a fault-tolerant CSS parser for PostCSS, which will find & fix syntax errors, capable of parsing any input).

--- template/build/webpack.prod.conf.js
+++ template/build/webpack.prod.conf.js
@@ -7,11 +7,12 @@ var merge = require('webpack-merge')
 var baseWebpackConfig = require('./webpack.base.conf')
 var CopyWebpackPlugin = require('copy-webpack-plugin')
 var HtmlWebpackPlugin = require('html-webpack-plugin')
-var ExtractTextPlugin = require('extract-text-webpack-plugin')
+var MiniCSSExtractPlugin = require('mini-css-extract-plugin')
 var OptimizeCSSPlugin = require('optimize-css-assets-webpack-plugin')
 var SWPrecacheWebpackPlugin = require('sw-precache-webpack-plugin')
 var loadMinified = require('./load-minified')
 var UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+var safeParser = require('postcss-safe-parser')
 
 var env = {{#if_or unit e2e}}process.env.NODE_ENV === 'testing'
   ? require('../config/test.env')
@@ -52,14 +53,15 @@ var webpackConfig = merge(baseWebpackConfig, {
     new webpack.DefinePlugin({
       'process.env': env
     }),
-    // extract css into its own file
-    new ExtractTextPlugin({
-      filename: utils.assetsPath('css/[name].[md5:contenthash:hex:15].css')
+    // mini css extract plugin
+    new MiniCSSExtractPlugin({
+      filename: 'common.[chunkhash].css'
     }),
     // Compress extracted CSS. We are using this plugin so that possible
     // duplicated CSS from different components can be deduped.
     new OptimizeCSSPlugin({
       cssProcessorOptions: {
+        parser: safeParser,
         safe: true
       }
     }),
